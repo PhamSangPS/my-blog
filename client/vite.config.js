90987857119d875f29e7973032d013a7265a29ec
PhamSangPS <160474426+PhamSangPS@users.noreply.github.com>
2024-03-13 10:03:03 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://my-blog-vj90.onrender.com/",
         secure: false,
       },
     },
